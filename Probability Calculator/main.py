15db5eb9678b175d08b7352afe485bc7e271f414
Boran Cihan Polat <bpolat502@gmail.com>
2023-07-19 21:55:15 +0200

    finished prob_calculator.py implemented test_module.py

--- Probability Calculator/main.py
+++ Probability Calculator/main.py
@@ -1,11 +1,16 @@
-from prob_calculator import Hat
+# This entrypoint file to be used in development. Start by reading README.md
+import prob_calculator
+from unittest import main
 
+prob_calculator.random.seed(95)
+hat = prob_calculator.Hat(blue=4, red=2, green=6)
+probability = prob_calculator.experiment(
+    hat=hat,
+    expected_balls={"blue": 2,
+                    "red": 1},
+    num_balls_drawn=4,
+    num_experiments=3000)
+print("Probability:", probability)
 
-hat = Hat(black=6, red=4, green=3)
-print(hat.contents)
-hat.draw(amount_of_balls_drawn=12)
-print(hat.draw(amount_of_balls_drawn=12))
-probability = experiment(hat=hat,
-                  expected_balls={"red":2,"green":1},
-                  num_balls_drawn=5,
-                  num_experiments=2000)
+# Run unit tests automatically
+main(module='test_module', exit=False)
--- Probability Calculator/prob_calculator.py
+++ Probability Calculator/prob_calculator.py
@@ -1,21 +1,16 @@
+import copy
+from collections import Counter
 import random
-
 
 class Hat():
     # with this method the infinite amount of arguments are directly
     # a dic
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-        keys = self.__dict__.keys()
-
+    def __init__(self, **balls):
+        balls = dict(balls)
         self.contents = []
 
-        for key in keys:
-            if key == "contents":
-                continue
-            else:
-                amount_of_key = self.__dict__[key]
-                self.contents.extend([key for i in range(amount_of_key)])
+        for color in balls:
+            self.contents.extend([color for i in range(balls[color])])
 
     def draw(self, amount_of_balls_drawn: int):
 
@@ -24,6 +19,35 @@ class Hat():
         else:
             removed_balls = []
             random_chosen_ele = random.sample(self.contents, amount_of_balls_drawn)
+            contents = self.contents
             for ele in random_chosen_ele:
                 removed_balls.append(ele)
+                contents.remove(ele)
         return removed_balls
+
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    experiments = []
+    for i in range(num_experiments):
+        hat_exp = copy.deepcopy(hat)
+        result = hat_exp.draw(num_balls_drawn)
+        result = dict(Counter(ball for ball in result))
+        experiments.append(result)
+
+    failed = []
+    for ball in list(expected_balls.keys()):
+        for r in experiments:
+            if not (ball in list(r.keys())):
+                failed.append(r)
+
+    experiments = [r for r in experiments if r not in failed]
+
+    failed = []
+    for ball in list(expected_balls.keys()):
+        for r in experiments:
+            if int(expected_balls[ball]) > int(r[ball]):
+                failed.append(r)
+
+    experiments = [r for r in experiments if r not in failed]
+
+    return len(experiments) / num_experiments
